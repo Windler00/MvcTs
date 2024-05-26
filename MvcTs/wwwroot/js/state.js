28d480c037e42014413f446c397007073502fe6f
Windler00 <Windler00@gmail.com>
2024-05-26 18:51:00 +0500

    added state implementation

---
+++ MvcTs/wwwroot/js/state.js
@@ -0,0 +1,37 @@
+﻿class Observable {
+    // Конструктор принимает начальное состояние и инициализирует массив подписчиков
+    constructor(initialState) {
+        this._state = initialState;
+        this.observers = [];
+    }
+
+    // Геттер для доступа к текущему состоянию
+    get state() {
+        return this._state;
+    }
+
+    // Сеттер для обновления состояния и уведомления подписчиков
+    set state(newState) {
+        this._state = newState;
+        this.notify(newState);
+    }
+
+    // Метод для подписки на изменения состояния
+    // Возвращает функцию для отписки
+    subscribe(observer) {
+        this.observers.push(observer);
+        return () => {
+            this.unsubscribe(observer);
+        };
+    }
+
+    // Метод для отписки от изменений состояния
+    unsubscribe(observer) {
+        this.observers = this.observers.filter(obs => obs !== observer);
+    }
+
+    // Метод для уведомления всех подписчиков об изменении состояния
+    notify(state) {
+        this.observers.forEach(observer => observer(state));
+    }
+}
